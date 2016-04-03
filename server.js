0dcaca8bb9adb4194a1b6fb66537d97f0baf46c2
Juncai <cai.jun@husky.neu.edu>
2016-04-02 23:54:25 -0400

    try to fix mongoDB on remote server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var bodyParser    = require('body-parser');
+var bodyParser = require('body-parser');
 // var session = require('session');
 var mongoose = require('mongoose');
 var app = express();
@@ -9,22 +9,24 @@ var connectionString = 'mongodb://127.0.0.1:27017/webdev2016';
 
 // use remote connection string
 // if running in remote server
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+if (process.env.OPENSHIFT_MONGODB_DB_HOST) {
+    connectionString = 'mongodb://' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
         process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-        process.env.OPENSHIFT_APP_NAME;
+        'webdev2016';
+    //connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+    //    process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+    //    process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+    //    process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+    //    process.env.OPENSHIFT_APP_NAME;
 }
 
 // connect to the database
 var db = mongoose.connect(connectionString);
 
 
-
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 require("./public/assignment/server/app.js")(app, db, mongoose);
