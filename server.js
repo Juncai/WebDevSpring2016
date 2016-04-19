434d472bbd2160c44b501cf7f6eabb04d5aeff25
Juncai <cai.jun@husky.neu.edu>
2016-04-19 14:44:58 -0400

    clean some code

--- server.js
+++ server.js
@@ -30,7 +30,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 multer();
 app.use(session({
-    //secret: 'this is the secret',
     secret: process.env.SESSION_SECRET,
     resave: true,
     saveUninitialized: true
